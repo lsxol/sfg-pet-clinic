246ab857855ade4c88d32fd23e930aed44a625b6
lsxol <d.kazmierczak96@gmail.com>
2022-08-05 20:39:54 +0200

    Refactoring to multimodule maven project and adding POJOs. closes #1 #2

---
+++ pet-clinic-data/src/main/java/guru.springframework.sfgpetclinic/model/Pet.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private Owner owner;
+    @lombok.Setter
+    public PetType petType;
+    private LocalDate birthDate;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
