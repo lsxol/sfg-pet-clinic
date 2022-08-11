64330428582be217c9d1699b75eb11ca34ead248
lsxol <d.kazmierczak96@gmail.com>
2022-08-11 23:20:11 +0200

    model changes #3

--- pet-clinic-data/src/main/java/guru.springframework.sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru.springframework.sfgpetclinic/model/Pet.java
@@ -3,10 +3,20 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity {
+
+    private String name;
     private Owner owner;
     @lombok.Setter
     public PetType petType;
     private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Owner getOwner() {
         return owner;
